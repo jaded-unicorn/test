4d0b86db333b19546fc0e918425b972d70babe3d
KB <jadedunicorn18@gmail.com>
2018-10-27 17:31:50 -0400

    cleanup #2

--- src/main/java/firstTest/application.java
+++ src/main/java/firstTest/application.java
@@ -9,6 +9,6 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+	firstTest.Application app = new Application();
     }
 }
