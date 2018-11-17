4b66f7ad6725e21a873389f4c298b21f495ca3c0
KB <jadedunicorn18@gmail.com>
2018-11-17 15:40:02 -0500

    adding date

--- src/main/java/firstTest/application.java
+++ src/main/java/firstTest/application.java
@@ -1,5 +1,9 @@
 package firstTest;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class application {
     
     public application() {
@@ -8,7 +12,15 @@ public class application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	//application app = new application();
+        System.out.println ("Starting the Application");
+    //application app = new application();
+
+        //Prints out the current date
+        //DateFormat dateFormat = new SimpleDateFormat("yyyy/dd/MM HH:mm:ss");
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+         Date date = new Date();
+        System.out.println(dateFormat.format(date)); //11/17/2018 15:27:25
+
+
     }
 }
